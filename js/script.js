4150725c5f8e6c33e9178386eedb301afe8ddd82
Mohd Rejoan <wearecrx@gmail.com>
2021-11-11 09:40:53 +0600

    ajax post added

--- js/script.js
+++ js/script.js
@@ -1,14 +1,42 @@
 $(document).ready(function () {
-  $.ajax({
-    url: "http://localhost:10010/wp-json/wp/v2/posts",
-    type: "GET",
-  }).done(function (data) {
-    data.forEach((item, index) => {
-      console.log(item);
-      console.log(index);
+    var postData = [];
+    // get all posts
+    $.getJSON(
+        "http://localhost:10010/wp-json/wp/v2/posts",
+        {
+            format: "json",
+        },
+        function (data) {
+          postData = data;
+          renderAllPost(postData);
+        }
+    );
+    // post render engine
+    const postRender = (data) => {
+        return `
+            <div class="item posts">
+                <h1 class="heading">${data.title.rendered}</h1>
+                <p class="para">${data.excerpt.rendered}</p>
+                <div class="bottom">
+                    <div class="left">
+                        <p>${data.date}</p>
+                    </div>
+                    <div class="right">
+                        <img src="./img/use-head.png" alt="user head short" />
+                        <p>julia robert</p>
+                    </div>
+                </div>
+            </div>
+    `;
+    };
+    // render all post on main container
+  const renderAllPost = (data) => {
+    $.each(data, function (index, dataItem) {
+      const posts = postRender(dataItem);
+      $("#main-container").append(posts);
     })
-  }).fail(function () {
-    console.log(this)
-    console.log("fail to get data");
-  })
+  }
+
+  // append texts 
+  
 });
