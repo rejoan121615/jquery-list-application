0e448a26a292a9138e5456672a03742d7cae6118
Mohd Rejoan <wearecrx@gmail.com>
2021-11-15 21:51:34 +0600

    log-in for validated

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
-$(document).ready(function () {
-  console.log("Everything working fine")
-})
+// log in form ---------------------
+const loginSubmitBnt = $("#log-in-form #submit");
+const loginInputList = $("#log-in-form .input-group input");
+
+// add input color
+
+function addColor(tag) {
+    if (tag.val() == "") {
+        $(tag).css("border-color", "red");
+    } else {
+        $(tag).css("border-color", "green");
+    }
+}
+
+
+
+$.map(loginInputList, function (items, index) {
+    // check input value
+    $(items).focus(function () {
+        addColor($(this));
+    });
+    $(items).blur(function () {
+        addColor($(this));
+    });
+
+    // acitvate submit btn
+  $(items).keypress(function () {
+    if (loginInputList[0].value != "" && loginInputList[1].value != "") {
+      $("#log-in-form #submit").attr("disabled", false);
+      } 
+    })
+});
