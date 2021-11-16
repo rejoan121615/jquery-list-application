3205a6380bb90eeafef905b2c2b8d481e38f1166
Mohd Rejoan <wearecrx@gmail.com>
2021-11-16 11:28:12 +0600

    user login details added

--- js/script.js
+++ js/script.js
@@ -12,8 +12,6 @@ function addColor(tag) {
     }
 }
 
-
-
 $.map(loginInputList, function (items, index) {
     // check input value
     $(items).focus(function () {
@@ -24,9 +22,43 @@ $.map(loginInputList, function (items, index) {
     });
 
     // acitvate submit btn
-  $(items).keypress(function () {
-    if (loginInputList[0].value != "" && loginInputList[1].value != "") {
-      $("#log-in-form #submit").attr("disabled", false);
-      } 
-    })
+    $(items).keypress(function () {
+        if (loginInputList[0].value != "" && loginInputList[1].value != "") {
+            $("#log-in-form #submit").attr("disabled", false);
+        }
+    });
 });
+
+// send log in user request
+function sendRequest() {
+    $.ajax({
+        url: "http://localhost:10010/wp-json/jwt-auth/v1/token",
+        method: "POST",
+        timeout: 0,
+        headers: {
+            "Content-Type": "application/json",
+        },
+        data: JSON.stringify({
+            username: loginInputList[0].value,
+            password: loginInputList[1].value,
+        }),
+    })
+        .done(function (respData, textStatus, jqXHR) {
+            window.localStorage.setItem("token", respData.token);
+            $("#log-in #log-in-form").fadeOut(500);
+            $("#log-in-success-msg").fadeIn(500);
+            setTimeout(function () {
+                window.location.href = "index.html";
+            }, 2500);
+        })
+        .fail(function (jqHRX, textStatus, error) {});
+}
+
+// on submit btn click
+$("#log-in-form #submit").click(function (e) {
+    e.preventDefault();
+    sendRequest();
+});
+
+
+// show user area 
